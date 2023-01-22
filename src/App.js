44ba6adb94a377af9199d8610a346dd4737af8f0
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2023-01-22 18:51:40 +0200

    скопировал и адаптировал html сайта

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
-let watermelon = "variable_flex";
+import {PizzaApp} from "./Components/PizzaApp";
 
 
 const App = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-                <div>
-                    Впервые делаем что-то на react'е за долгое время.
-                    <br/>
-                    Йаху. Хеллоу ворлд! {`${2+2} арбуз ${watermelon}`}
-                </div>
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
-    </div>
-);
+    <PizzaApp/>
+)
 
 export default App;
