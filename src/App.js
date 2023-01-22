7377b864ff171f672fded5355b6cbf9480c609dc
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2023-01-22 17:43:46 +0200

    Тест

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+let watermelon = "variable_flex";
+
+
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+                <div>
+                    Впервые делаем что-то на react'е за долгое время.
+                    <br/>
+                    Йаху. Хеллоу ворлд! {`${2+2} арбуз ${watermelon}`}
+                </div>
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
     </div>
-  );
-}
+);
 
 export default App;
