841ef13b76e0a088fa23ced046bfcc8b5d7d1589
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2023-01-24 20:11:55 +0200

    Разбил на компоненты. Вспомнил как юзать пропсы

---
+++ src/сomponents/TopBar.jsx
@@ -0,0 +1,9 @@
+import {Sort} from "./Sort";
+import {Categories} from "./Categories";
+
+export let TopBar = () => (
+    <div className="content__top">
+        <Categories/>
+        <Sort/>
+    </div>
+)
